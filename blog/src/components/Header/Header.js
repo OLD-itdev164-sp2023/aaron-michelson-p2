a64105ad14d9c5aae1d859a880fc6bb73d31bf01
ajmichel4000 <93304691+ajmichel4000@users.noreply.github.com>
2023-05-06 14:32:10 -0500

    Project 2 Completed

--- blog/src/components/Header/Header.js
+++ blog/src/components/Header/Header.js
@@ -2,19 +2,19 @@ import * as React from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
 import styled from 'styled-components'
+import { Search } from 'styled-icons/feather'
+import { H1 } from '../Heading'
+import { IconButton } from '../Button'
+import { Section } from '../Section'
 
 const Outer = styled.header`
-  background: ${props => props.theme.header.backgroundColor};
+  background: ${({ theme }) => theme.variants.header.primary.backgroundColor};
   margin-bottom: 1.45rem;
   margin: 0 auto;
   padding: var(--space-4) var(--size-gutter);
   display: flex;
   align-items: center;
   justify-content: space-between;
-`
-
-const H1 = styled.h1`
-  margin: 0px;
 `
 
 const StyledLink = styled(Link)`
@@ -27,11 +27,16 @@ const StyledLink = styled(Link)`
 
 const Header = ({ siteTitle }) => (
   <Outer>
-    <H1>
-      <StyledLink to="/">
-        {siteTitle}
-      </StyledLink>
-    </H1>
+    <Section width={11/12}>
+      <H1>
+        <StyledLink to="/">
+          {siteTitle}
+        </StyledLink>
+      </H1>
+    </Section>
+    <Section width={1/12}>
+      <IconButton icon={<Search />} variant='contrast'/>
+    </Section>
   </Outer>
 )
 
@@ -43,4 +48,4 @@ Header.defaultProps = {
   siteTitle: ``,
 }
 
-export default Header
+export { Header }
